8553a56720fdd528daf1e4446dad6783064184ff
Chakirou ALABANI <chakirou.pro@gmail.com>
2024-11-19 14:22:21 -0500

    forgot to push code, now done!

---
+++ code/run.sh
@@ -0,0 +1,4 @@
+#python3 codemain.py -op examples/output/real/97/FAM86 -palnf examples/input/FAM86_2_result.txt -sf examples/input/FAM86_2_initialsource.fasta -tf examples/input/FAM86_2_target.fasta -s2tf examples/input/FAM86_2_initialsource2target.txt 
+
+
+# python3 src_python3/main.py -op examples/output/real/97/FAM86 -of listaln -sf examples/input/FAM86_2_initialsource.fasta -tf examples/input/FAM86_2_target.fasta -s2tf examples/input/FAM86_2_initialsource2target.txt  -palnf examples/input/FAM86_2_result.txt
